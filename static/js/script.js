8283bcc95d5e87a10b489e05221de269bfc7c36a
rajesh <rajesh.ekula@gmail.com>
2018-08-13 15:19:38 +0530

    fixes bugs

--- static/js/script.js
+++ static/js/script.js
@@ -6,10 +6,6 @@
 			$scope.series = [];
 			$scope.data = [];
 
-			$scope.onClick = function (points, evt) {
-				console.log(points, evt);
-			};
-			
 			$scope.exportImg = function($event) {
 				var dataURL = document.querySelector("#line").toDataURL("image/png");
 				document.getElementById('img-link').href=dataURL;
@@ -26,17 +22,23 @@
 
 		angular.module("app").controller('DoughnutCtrl',['$scope','$http',DoughnutCtrl]);
 		function DoughnutCtrl($scope,$http){
-			$scope.labels = [];
+			$scope.labels = ['Buses','Four wheelers','Goods vehicles','others','Two wheelers'];
 			$scope.data = [];
-
-			$http.get("https://api.population.io:80/1.0/population/2018/aged/18/").
+			$scope.temp = [];
+			$scope.buses = 0;
+			$scope.four = 0;
+			$scope.goods = $scope.others = $scope.two = 0;
+			$http.get("https://api.data.gov.in/resource/7a82b08b-d7fe-4edb-82f7-216a1f1bed07?api-key=579b464db66ec23bdd000001cdd3946e44ce4aad7209ff7b23ac571b&format=json&offset=0&limit=10").
 				then(function(data) {
-					angular.forEach(data.data, function(chartDoughnutData,key) {
-						if (key <=9){
-							$scope.data.push(chartDoughnutData.total);
-							$scope.labels.push(chartDoughnutData.country);
-						}
+					angular.forEach(data.data.records, function(chartDoughnutData,key) {
+						$scope.buses += parseInt(chartDoughnutData.buses_);
+						$scope.four += parseInt(chartDoughnutData.cars_jeeps_and_taxis);
+						$scope.goods += parseInt(chartDoughnutData.goods_vehicles);
+						$scope.others += parseInt(chartDoughnutData.others_);
+						$scope.two += parseInt(chartDoughnutData.two_wheelers_);
+
 					});
+					$scope.data.push($scope.buses,$scope.four,$scope.goods,$scope.others,$scope.two);
 				});
 			$scope.exportImg = function($event) {
 				var dataURL = document.querySelector("#doughnut").toDataURL("image/png");
@@ -63,8 +65,7 @@
 							$scope.labels.push("Age"+chartBarData.age);
 						}
 					});
-					$scope.data.push($scope.males);
-					$scope.data.push($scope.females);
+					$scope.data.push($scope.males,$scope.females);
 				});
 
 			$scope.exportImg = function($event) {
